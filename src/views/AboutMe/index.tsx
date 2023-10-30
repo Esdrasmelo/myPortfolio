d7517abdc2c1910c94cd717dc8caf3e3224aaa91
Esdras de Melo Motta <emelomotta@gmail.com>
2023-10-29 21:11:21 -0300

    refactor: fix writing (#3)

--- src/views/AboutMe/index.tsx
+++ src/views/AboutMe/index.tsx
@@ -10,20 +10,20 @@ function AboutMe() {
         <h1>Muito Prazer!</h1>
         <SlideInParagraph>
           Me chamo Esdras de Melo Motta e <b>atuo</b> como
-          <b>desenvolvedor back-end</b> desde os meus <b>16 anos</b>.
+          <b> desenvolvedor back-end</b> desde os meus <b>16 anos</b>.
         </SlideInParagraph>
         <SlideInParagraph>
           Sempre fui muito <b>curioso</b> e, por conta disso, desde meus{" "}
           <b>9 anos</b> de idade sou <b>apaixonado</b> por
           <b> tecnologia</b> e por tudo que envolva
-          <b> criação de aplicações</b>.
+          <b> criação de aplicações</b> e <b>soluções para a web</b>.
         </SlideInParagraph>
         <SlideInParagraph>
           Ao longo do tempo <b>atuando</b> como <b>desenvolvedor back-end</b>,
           tive a oportunidade de trabalhar em mais de <b>10 projetos</b> que
           puderam <b> agregar </b>
-          muito à minha <b>carreira</b>, ajudando-me a{" "}
-          <b>aprender coisas novas</b> e a lidar com <b>desafios</b> e{" "}
+          muito à minha <b>carreira</b>, ajudando-me a ter{" "}
+          <b>diversos aprendizados</b> e a lidar com <b>desafios</b> e{" "}
           <b>contextos</b> diferentes em cada <b>cliente</b>.
         </SlideInParagraph>
       </SlideInSection>
